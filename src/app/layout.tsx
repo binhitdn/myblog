e173e26f7297aba78e36976bddc80827b7bb3b21
Bình Phan <binhpc.21it@vku.udn.vn>
2023-06-11 12:48:14 +0700

    first commt222

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="BinhDevBlog" />
+        <meta name="author" content="BinhDev" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </head>
       <body className="text-primary bg-primary relative mx-auto mb-20 flex w-full max-w-screen-xl flex-col px-[10vw] md:px-[5vw]">
         <Provider>
           <Header />
